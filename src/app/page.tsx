5ef6dfdf1cd49f8747e4a55aa915860753e23d6b
fillwo <dias.felipecarlos@gmail.com>
2024-03-30 11:44:26 -0300

    atualização vouch3

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import Menu from "@/components/Menu";
+import { Header } from "@/components/Header";
+export default function Home() {
+  return (
+    <>
+<Header/>
+<Menu/>
+    </>
+  )}
---
+++ src/app/verification/page.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Menu from "@/components/Menu";
+
+export const Verification =()=>{
+    return(
+        <>
+          <Menu/>
+       <div className="min-h-screen flex flex-col  bg-[#1a191b] ">
+       
+       <a className=" relative mt-16 text-center  text-slate-50 font-medium text-xl  ">Verification</a>
+       
+      
+
+<div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+   
+</div>
+
+      
+       
+       
+       </div>
+       
+   
+        </>
+    );
+};
+export default Verification
